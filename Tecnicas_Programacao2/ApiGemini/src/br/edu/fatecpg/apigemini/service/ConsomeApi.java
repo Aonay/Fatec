7e89a7bed588709aa1ee3683fb01e8e0dbee7d12
AlanMatias <alan.rodrigues.m@gmail.com>
2024-09-19 00:16:06 -0300

    :lock:  Protegendo APIKEY

--- Tecnicas_Programacao2/ApiGemini/src/br/edu/fatecpg/apigemini/service/ConsomeApi.java
+++ Tecnicas_Programacao2/ApiGemini/src/br/edu/fatecpg/apigemini/service/ConsomeApi.java
@@ -8,8 +8,10 @@ import java.net.http.HttpResponse;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 public class ConsomeApi {
-    private  static final String API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyCINhUFUMA7j1fOVWaLOWeTD-8gFzvCIH0";
+    private static final String API_KEY = System.getenv("GOOGLE_API_KEY");
+    private  static final String API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key="+ API_KEY;
     private static final Pattern RESP_PATTERN = Pattern.compile("\"text\"\\s*:\\s*\"([^\"]+)\"");
     public static String fazerPergunta(String pergunta) throws IOException, InterruptedException {
         String jsonRequest = gerarJsonRequest(pergunta);
