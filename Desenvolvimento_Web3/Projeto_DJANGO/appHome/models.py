0b19e11df3acc95d0323e393421a48d16fabbf83
AlanMatias <alan.rodrigues.m@gmail.com>
2024-09-20 01:57:40 -0300

    :card_file_box: Formulario e BD

--- Desenvolvimento_Web3/Projeto_DJANGO/appHome/models.py
+++ Desenvolvimento_Web3/Projeto_DJANGO/appHome/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+  nome = models.CharField(max_length=255)
+  sobrenome = models.CharField(max_length=255)
+  idade = models.IntegerField()
+  curso = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.nome
--- Desenvolvimento_Web3/Projeto_DJANGO/appHome/urls.py
+++ Desenvolvimento_Web3/Projeto_DJANGO/appHome/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 		path('' , views.appHome, name="appHome"),
+    path('criar/',views.criar_usuario,name='criar_usuario'),
+    path('usuarios/',views.lista_usuarios, name='lista_usuarios'),
 ]
