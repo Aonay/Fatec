5a7c8db308b92f04e2023b3b497eb3ea9056f09f
AlanMatias <alan.rodrigues.m@gmail.com>
2024-10-09 21:34:07 -0300

    Removento classe de interface

--- Tecnicas_Programacao2/Pratica6_StreamLambda/src/br/edu/fatecpg/tp2/services/IConverteDado.java
+++
@@ -1,7 +0,0 @@
-package br.edu.fatecpg.tp2.services;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-public interface IConverteDado {
-    <T> T obterDato(String json,Class<T> classe)throws JsonProcessingException;
-}
